fc7bca027326f7b4d83c27684648fd2c6f88ceca
Santiago Zubieta <santiago.zubieta@gmail.com>
2016-02-12 07:09:23 -0500

    Changes to descriptive comments in simulator structs file

--- command/simulator/simulator_structs.go
+++ command/simulator/simulator_structs.go
@@ -7,7 +7,7 @@ import (
 
 // This struct will hold the present state of all the nodes, jobs and allocations in a single
 // iteration (each Job Evaluation), which we'll use to analyse the Schedulers.
-type SimulatorSnapshot struct {
+type SimSnapshot struct {
 	// The Evaluation associated to the Job processed during a given iteration.
 	Eval *structs.Evaluation
 	// The Plans that have been created.
@@ -16,45 +16,40 @@ type SimulatorSnapshot struct {
 	State *state.StateStore
 	// The list of Node IDs inside of the cluster at a given iteration.
 	NodeIDs []string
-	// The UNIX timestamp at the moment of processing the job. This together with the allocation
-	// time is helpful for determining the real order of job processings given that each processing
+	// The UNIX timestamp at the moment of processing the Job. This together with the allocation
+	// time is helpful for determining the real order of Job processings given that each processing
 	// is done in a separate goroutine.
 	Time int64
 }
 
-// This struct will hold the metrics of a single iteration, which will be later serialized
-// and outputted by the program.
-type Node struct {
-	// The ID of this Node
-	ID string `json:"id"`
-	// Datacenter for this Node
-	Datacenter string `json:"datacenter"`
-	// Node Name
-	Name string `json:"name"`
-	// Node Attributes
+// This struct will hold the metrics of a single Node, which will be later serialized
+// and outputted by the program. These are the initial Node values without usage.
+type SimNode struct {
+	ID         string            `json:"id"`
+	Datacenter string            `json:"datacenter"`
+	Name       string            `json:"name"`
 	Attributes map[string]string `json:"attributes"`
-	// The resources that the Node has by default.
-	Resources *SimulatorResources `json:"resources"`
-	// The reserved resources of the Node.
-	Reserved *SimulatorResources `json:"reserved"`
+	Resources  *SimResources     `json:"resources"`
+	Reserved   *SimResources     `json:"reserved"`
 }
 
-type NodeUsage struct {
-	// The ID of this Node
-	ID string `json:"id"`
-	// The currently available Resources for this Node.
-	Available *SimulatorResources `json:"available"`
+// This struct will hold the updates of usage of a single Node. These values change
+// after Allocations, so associate these to Job evaluations.
+type NodeUsageChange struct {
+	ID        string        `json:"id"`
+	Available *SimResources `json:"available"`
 }
 
-// SimulatorResources is used to briefly define the resources available on a client.
-type SimulatorResources struct {
+// SimResources is used to briefly define the resources of a Node, whether total,
+// reserved, or the ones left available when new Allocations happen.
+type SimResources struct {
 	CPU      int `hcl:"cpu"       json:"cpu"`
 	MemoryMB int `hcl:"memory_mb" json:"memory_mb"`
 	DiskMB   int `hcl:"disk_mb"   json:"disk_mb"`
 	IOPS     int `hcl:"iops"      json:"iops"`
 }
 
-// A certain relevant metrics (for now) for a given Job.
+// A certain relevant metrics (as of now) for a given Job.
 type JobMetrics struct {
 	ID             string   `json:"job_id"`
 	Datacenters    []string `json:"datacenters"`
@@ -66,32 +61,34 @@ type JobMetrics struct {
 	FinalTimestamp int64    `json:"final_time_stamp"`
 }
 
-// A certain relevant metrics (for now) for a given Allocation.
+// A certain relevant metrics (as of now) for a given Allocation.
 type AllocMetrics struct {
-	NodeID             string              `json:"node_id"`
-	JobID              string              `json:"job_id"`
-	TaskGroup          string              `json:"task_group"`
-	AllocationTime     int64               `json:"allocation_time"`
-	DesiredDescription string              `json:"desired_description"`
-	DesiredStatus      string              `json:"desired_status"`
-	Resources          *SimulatorResources `json:"resources"`
+	NodeID             string        `json:"node_id"`
+	JobID              string        `json:"job_id"`
+	TaskGroup          string        `json:"task_group"`
+	AllocationTime     int64         `json:"allocation_time"`
+	DesiredDescription string        `json:"desired_description"`
+	DesiredStatus      string        `json:"desired_status"`
+	Resources          *SimResources `json:"resources"`
 }
 
-// Get the most relevant Node information from a Node struct.
-// Relevant as in helpful for evaluating placement and bin packing.
-func parseNode(node *structs.Node) *Node {
-	return &Node{
+// Get the only most relevant Node information from a Node struct.
+// Serializing the whole Node could be expensive, for now lets just
+// focus on what we deem relevant for evaluating placements and the
+// bin packing of the simulator.
+func parseNode(node *structs.Node) *SimNode {
+	return &SimNode{
 		ID:         node.ID,
 		Name:       node.Name,
 		Datacenter: node.Datacenter,
 		Attributes: node.Attributes,
-		Resources: &SimulatorResources{
+		Resources: &SimResources{
 			CPU:      node.Resources.CPU,
 			MemoryMB: node.Resources.MemoryMB,
 			DiskMB:   node.Resources.DiskMB,
 			IOPS:     node.Resources.IOPS,
 		},
-		Reserved: &SimulatorResources{
+		Reserved: &SimResources{
 			CPU:      node.Reserved.CPU,
 			MemoryMB: node.Reserved.MemoryMB,
 			DiskMB:   node.Reserved.DiskMB,
@@ -100,9 +97,14 @@ func parseNode(node *structs.Node) *Node {
 	}
 }
 
-func getAvailable(node *structs.Node, state *state.StateStore) *SimulatorResources {
+// For a given Node, lets calculate the resources it has left available, after
+// substracting from its original resources the consumption done by Allocations
+// associated to such Node.
+func getAvailable(node *structs.Node, state *state.StateStore) *SimResources {
 
-	available := &SimulatorResources{
+	// The original resources available for Allocations are the base resources
+	// of the Node, minus the resources that are reserved.
+	available := &SimResources{
 		CPU:      node.Resources.CPU - node.Reserved.CPU,
 		MemoryMB: node.Resources.MemoryMB - node.Reserved.MemoryMB,
 		DiskMB:   node.Resources.DiskMB - node.Reserved.DiskMB,
@@ -112,15 +114,14 @@ func getAvailable(node *structs.Node, state *state.StateStore) *SimulatorResourc
 	// Get the allocations associated with the current node
 	allocations, _ := state.AllocsByNode(node.ID)
 	for _, allocation := range allocations {
-		// And then the resources consumed by such association, to reflect them on the current
-		// node resource consumption.
+		// And then substract the resources consumed by each Allocation, to
+		// reflect them on the current node resource consumption.
 		resources := allocation.Resources
 		available.CPU = available.CPU - resources.CPU
 		available.MemoryMB = available.MemoryMB - resources.MemoryMB
 		available.DiskMB = available.DiskMB - resources.DiskMB
 		available.IOPS = available.IOPS - resources.IOPS
 	}
-
 	return available
 }
 
@@ -131,7 +132,6 @@ func ParseJobMetrics(job *structs.Job) *JobMetrics {
 	for _, taskGroup := range job.TaskGroups {
 		taskGroups = append(taskGroups, taskGroup.Name)
 	}
-
 	return &JobMetrics{
 		ID:          job.ID,
 		Region:      job.Region,
@@ -148,7 +148,7 @@ func ParseAllocMetrics(alloc *structs.Allocation) *AllocMetrics {
 	return &AllocMetrics{
 		NodeID: alloc.NodeID,
 		JobID:  alloc.JobID,
-		Resources: &SimulatorResources{
+		Resources: &SimResources{
 			CPU:      alloc.Resources.CPU,
 			MemoryMB: alloc.Resources.MemoryMB,
 			DiskMB:   alloc.Resources.DiskMB,
@@ -163,14 +163,14 @@ func ParseAllocMetrics(alloc *structs.Allocation) *AllocMetrics {
 
 // The metrics of the cluster for a given iteration ~ Job processing.
 type JobEvaluationMetrics struct {
-	JobMetrics              *JobMetrics     `json:"evaluated_job"`
-	SuccessfulAllocsMetrics []*AllocMetrics `json:"successful_allocs_metrics"`
-	FailedAllocsMetrics     []*AllocMetrics `json:"failed_allocs_metrics"`
-	NodeUsageChanges        []*NodeUsage    `json:"node_usage_changes"`
+	JobMetrics              *JobMetrics        `json:"evaluated_job"`
+	SuccessfulAllocsMetrics []*AllocMetrics    `json:"successful_allocs_metrics"`
+	FailedAllocsMetrics     []*AllocMetrics    `json:"failed_allocs_metrics"`
+	NodeUsageChanges        []*NodeUsageChange `json:"node_usage_changes"`
 }
 
 // The output of the simulator after processing all Jobs.
-type SimulatorOutput struct {
-	Nodes          []*Node                 `json:"nodes"`
+type SimOutput struct {
+	Nodes          []*SimNode              `json:"nodes"`
 	JobEvaluations []*JobEvaluationMetrics `json:"job_evaluations"`
 }
