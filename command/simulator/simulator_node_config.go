8a6f5900a1ffb8ba9ea1ff554b0c635e6817be2a
Santiago Zubieta <santiago.zubieta@gmail.com>
2016-02-12 05:59:08 -0500

    Added support for Node config files written in JSON

--- command/simulator/simulator_node_config.go
+++ command/simulator/simulator_node_config.go
@@ -1,6 +1,8 @@
 package simulator
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 
 	"github.com/hashicorp/hcl"
@@ -12,60 +14,60 @@ import (
 // Node struct in 'structs', but we don't know if its a good idea to make
 // this kind of changes outside of the simulator package (it should adapt
 // to Nomad's logic, not the other way around), since in production nodes'
-// configurations aren't retrieved from a config file but from the system
-// itself via fingerprinting.
+// structs aren't retrieved from a config file but from the system itself
+// via fingerprinting. It will be read from either HCL or JSON files.
 type NodeConfig struct {
 	// The ID of this Node
-	ID string `hcl:"id"`
+	ID string `hcl:"id" json:"id"`
 
 	// Datacenter for this Node
-	Datacenter string `hcl:"datacenter"`
+	Datacenter string `hcl:"datacenter" json:"datacenter"`
 
 	// Node name
-	Name string `hcl:"name"`
+	Name string `hcl:"name" json:"name"`
 
 	// Attributes is an arbitrary set of key/value data that can be used for
 	// defining constraints or available task drivers. Examples
 	// include "kernel.name=linux", "arch=386", "driver.docker=1",
 	// "driver.exec=1", "docker.runtime=1.8.3"
-	Attributes map[string]string `hcl:"attributes"`
+	Attributes map[string]string `hcl:"attributes" json:"attributes"`
 
 	// Resources is the available resources on the client.
 	// For example "cpu=2700" "memory=8192"
-	Resources *SimulatorResources `hcl:"resources"`
+	Resources *SimulatorResources `hcl:"resources" json:"resources"`
 
 	// Reserved is the set of Resources that are reserved, and should be
 	// subtracted from the total Resources for the purposes of scheduling.
 	// This may be provide certain high-watermark tolerances or because
 	// of external schedulers consuming resources.
-	Reserved *SimulatorResources `hcl:"reserved"`
+	Reserved *SimulatorResources `hcl:"reserved" json:"reserved"`
 
 	// Links are used to 'link' this client to external systems.
 	// For example "consul=foo.dc1" "aws=i-83212" "ami=ami-123"
-	Links map[string]string `hcl:"links"`
+	Links map[string]string `hcl:"links" json:"links"`
 
 	// Meta is used to associate arbitrary metadata with this client.
 	// This is opaque to Nomad.
-	Meta map[string]string `hcl:"meta"`
+	Meta map[string]string `hcl:"meta" json:"meta"`
 
 	// NodeClass is an opaque identifier used to group nodes together
 	// for the purpose of determining scheduling pressure.
-	NodeClass string `hcl:"node_class"`
+	NodeClass string `hcl:"node_class" json:"node_class"`
 
 	// Drain is controlled by the servers, and not the client. If true, no jobs
 	// will be scheduled to this node, and existing allocations will be drained.
-	Drain bool `hcl:"drain"`
+	Drain bool `hcl:"drain" json:"drain"`
 
 	// Status of this Node
 	// For example: "initializing", "ready", "down"
-	Status string `hcl:"status"`
+	Status string `hcl:"status" json:"status"`
 
 	// StatusDescription is meant to provide more human useful information
-	StatusDescription string `hcl:"status_description"`
+	StatusDescription string `hcl:"status_description" json:"status_description"`
 
 	// Raft Indexes
-	CreateIndex uint64 `hcl:"create_index"`
-	ModifyIndex uint64 `hcl:"modify_index"`
+	CreateIndex uint64 `hcl:"create_index" json:"create_index"`
+	ModifyIndex uint64 `hcl:"modify_index" json:"modify_index"`
 }
 
 // Get the regular Node struct from the NodeConfig struct (intermediary for
@@ -122,11 +124,12 @@ func ConvertNode(n *NodeConfig) *structs.Node {
 	}
 }
 
-// LoadNodeString is used to load a Node from a config string.
-func LoadNodeString(s string) (*structs.Node, error) {
+// Parse a NodeConfig struct from a HCL file.
+func parseHCL(c []byte) (*NodeConfig, error) {
+	// Up to this point, the file is read as a string, we need to parse
+	// it and return the resulting struct.
+	s := string(c)
 	// Parse!
-	// TODO: add JSON parsing. And figure out how to decide whether to
-	// apply HCL or JSON parsing to the file string.
 	obj, err := hcl.Parse(s)
 	if err != nil {
 		return nil, err
@@ -138,18 +141,47 @@ func LoadNodeString(s string) (*structs.Node, error) {
 		return nil, err
 	}
 
-	return ConvertNode(&result), nil
+	return &result, nil
 }
 
-// LoadNodeConfigFile loads the node configuration from the given file path.
+// Parse a NodeConfig struct from a JSON file.
+func parseJSON(c []byte) (*NodeConfig, error) {
+	// Start building the result, for now its our intermediate struct.
+	var result NodeConfig
+	if err := json.Unmarshal(c, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+// LoadNodeConfigFile loads the Node struct from the given file path.
 func LoadNodeFile(path string) (*structs.Node, error) {
+	// Start building the result, for now its our intermediate struct.
+	var result *NodeConfig
+
 	// Read the file
 	c, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	// Up to this point, the file is read as a string, we need to parse
-	// it and return the resulting struct.
-	return LoadNodeString(string(c))
+	length := len(path)
+	// Parse HCL if the extension is ".hcl", or JSON if extension is ".json"...
+	// or return an error if its neither of those extensions.
+	if length > 4 && path[length-4:] == ".hcl" {
+		result, err = parseHCL(c)
+		if err != nil {
+			return nil, err
+		}
+	} else if length > 5 && path[length-5:] == ".json" {
+		result, err = parseJSON(c)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, fmt.Errorf("unrecognized extension: %v", path)
+	}
+
+	return ConvertNode(result), nil
 }
