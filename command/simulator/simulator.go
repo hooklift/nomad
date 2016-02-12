a1fa5f476a272d17b92066406a482cab3cdf26fd
Santiago Zubieta <santiago.zubieta@gmail.com>
2016-02-12 05:23:07 -0500

    Final details on main simulator.go file, mostly comments

--- command/simulator/simulator.go
+++ command/simulator/simulator.go
@@ -6,97 +6,87 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/hashicorp/nomad/command"
 	"github.com/hashicorp/nomad/jobspec"
 	"github.com/hashicorp/nomad/nomad/structs"
 	"github.com/hashicorp/nomad/scheduler"
+
+	"github.com/hashicorp/nomad/command"
 )
 
-type SimulatorCommand struct {
+type SimCommand struct {
 	command.Meta
 }
 
-func (c *SimulatorCommand) Help() string {
+func (c *SimCommand) Help() string {
 	helpText := `
 Usage: nomad simulator -nodeList <path> -jobList <path> -outFile <path>
-
   -nodeList <path>
     A path to a file containing a list of node specification file paths, one per line.
-
   -jobList <path>
     A path to a file containing a list of job specification file paths, one per line.
-    Jobs will be iteratively registered in the order they're listed.
-
   -outFile <path>
     A path where the output JSON file will be created into. If not specified, default
     filename is simulator_output.json, at the location where binary was run.
-
     Example:
       $ nomad simulator -nodeList nodes.txt -jobList jobs.txt
 `
 	return strings.TrimSpace(helpText)
 }
 
-func (c *SimulatorCommand) Synopsis() string {
+func (c *SimCommand) Synopsis() string {
 	return "Run a simulator to get metrics about the placement of jobs"
 }
 
-func (c *SimulatorCommand) Run(args []string) int {
+func (c *SimCommand) Run(args []string) int {
 
 	// A flag which will contain as a single string the path to a file containing nodes' configuration file paths.
-	var nodeListFlag string
+	var nodeListPath string
 	// Nodes' paths parsed from nodes' flag string.
 	var nodesPaths []string
 	// A flag which will contain as a single string the path to a file containing jobs' configuration file paths.
-	var jobListFlag string
+	var jobListPath string
 	// Jobs' paths parsed from jobs' flag string.
 	var jobsPaths []string
-	// A flag which will contain the path for the result file.
-	var outFileFlag string
 	// Path for the output file.
-	var outFilePath string
+	var outFile string
 
-	// All the given jobs will be attempted to be placed on the nodes iteratively, until
-	// it finishes and it will output a JSON file with the 'logs' describing the cluster state
-	// at each iteration, which can be used later for analysis.
-	flags := flag.NewFlagSet("simulator", flag.ContinueOnError)
+	flags := flag.NewFlagSet("sim", flag.ContinueOnError)
 	flags.Usage = func() { c.Ui.Error(c.Help()) }
 
-	flags.StringVar(&nodeListFlag, "nodeList", "", "nodeList")
-	flags.StringVar(&jobListFlag, "jobList", "", "jobList")
-	flags.StringVar(&outFileFlag, "outFile", "", "outFile")
+	flags.StringVar(&nodeListPath, "nodeList", "", "nodeList")
+	flags.StringVar(&jobListPath, "jobList", "", "jobList")
+	flags.StringVar(&outFile, "outfile", "", "outfile")
 
 	if err := flags.Parse(args); err != nil {
 		return 1
 	}
 
 	// Retrieve the nodes' config file paths from the file given as argument.
-	if nodeListFlag != "" {
+	if nodeListPath != "" {
 		var err error
-		nodesPaths, err = readPaths(nodeListFlag)
+		nodesPaths, err = readPaths(nodeListPath)
 		noErr(err)
 	} else {
 		return 1
 	}
 
 	// Retrieve the jobs' config file paths from the file given as argument.
-	if jobListFlag != "" {
+	if jobListPath != "" {
 		var err error
-		jobsPaths, err = readPaths(jobListFlag)
+		jobsPaths, err = readPaths(jobListPath)
 		noErr(err)
 	} else {
 		return 1
 	}
 
-	if outFileFlag != "" {
-		outFilePath = outFileFlag
-	} else {
-		outFilePath = "simulator_output.json"
+	if outFile == "" {
+		outFile = "simulator_output.json"
 	}
 
 	// Stores the Nodes' pointers.
@@ -104,7 +94,8 @@ func (c *SimulatorCommand) Run(args []string) int {
 	// Stores the the Jobs' pointers.
 	var jobs []*structs.Job
 
-	// Stores the node IDs, useful for retrieving Nodes from the harness internal state.
+	// Stores the node IDs, useful for retrieving Nodes from the harness internal state
+	// and Allocations related to them.
 	var nodeIDs []string
 
 	// Load the Nodes' configuration files.
@@ -114,9 +105,7 @@ func (c *SimulatorCommand) Run(args []string) int {
 			c.Ui.Error(fmt.Sprintf("Error loading configuration from %s: %s", nodePath, err))
 			return 1
 		}
-
 		nodes = append(nodes, current)
-		nodeIDs = append(nodeIDs, current.ID)
 	}
 
 	// Load the Jobs' configuration files.
@@ -126,16 +115,6 @@ func (c *SimulatorCommand) Run(args []string) int {
 			c.Ui.Error(fmt.Sprintf("Error parsing job file %s: %s", jobPath, err))
 			return 1
 		}
-
-		// The usual rules apply for allocation:
-		// Jobs must belong to same region of nodes, otherwise won't allocate.
-		// Jobs must have a datacenter related to the one in the nodes, otherwise won't allocate.
-		// Jobs' task drivers must be related to the one in nodes, otherwise won't allocate.
-		// ...
-		// For specifically evaluating the bin packing, its better if all jobs and nodes belong
-		// to a single datacenter, in the same region, with the same task drivers, so all nodes
-		// are eligible for scheduling, and the bin packing can be seen more evidently.
-
 		jobs = append(jobs, current)
 	}
 
@@ -144,31 +123,51 @@ func (c *SimulatorCommand) Run(args []string) int {
 	// specific features, in our case, scheduling and placement simulation,
 	// but with user inputted configurations instead of mocked values.
 	h := NewSimulatorHarness()
+	// Initialize the mutex. This will lock Job scheduling operations.
+	// Without this, and while using the harness logic, a problem arises in
+	// which two Jobs are being concurrently allocated and they result in a
+	// situation of resource over-consumption for a given Node.
+	mutex := &sync.Mutex{}
 
-	// Upsert to the Harness Nodes parsed from configuration files.
 	for _, node := range nodes {
 		noErr(h.State.UpsertNode(h.NextIndex(), node))
+		nodeIDs = append(nodeIDs, node.ID)
 	}
 
+	log.Println("Running Nomad simulator...")
+
 	// Jobs will be processed iteratively in the order they are present at
-	// input. After each Job is processed, metrics will be extracted from
-	// the present cluster state. This extraction of metrics is done in a
-	// concurrent fashion while other jobs are processing.
+	// input, but each one in a separate goroutine, so the real order in
+	// which they finish processing is variable.
+	// After each Job is processed, metrics will be extracted from
+	// the present cluster state.
 	// ...
 	// This is a channel where a snapshot of the simulator will be sent for
 	// metrics extraction after each job has processed.
 	snapshotChan := make(chan *SimulatorSnapshot)
 
-	mutex := &sync.Mutex{}
-
 	// For each job...
 	for _, job := range jobs {
+		// Run a separate goroutine...
 		go func(job *structs.Job) {
 
-			// Upsert to the Harness Jobs parsed from configuration files.
+			// Here's where starts what counts as 'processing' a Job, which comprises
+			// adding it to the State, and then scheduling it. Start counting time from
+			// this point in time, until it is done scheduling.
+			startTimestamp := int64(time.Now().UnixNano())
+
+			// Upsert into the Harness Jobs parsed from configuration files.
 			noErr(h.State.UpsertJob(h.NextIndex(), job))
 
 			// Create mock Evaluations to register the Jobs.
+			// The usual rules apply for allocation:
+			// Jobs must belong to same region of nodes, otherwise won't allocate.
+			// Jobs must have a datacenter related to the one in the nodes, otherwise won't allocate.
+			// Jobs' task drivers must be related to the ones in nodes, otherwise won't allocate.
+			// ...
+			// For specifically evaluating the bin packing, its better if all jobs and nodes belong
+			// to a single datacenter, in the same region, with the same task drivers, so all nodes
+			// are eligible for scheduling, and the bin packing can be seen more evidently.
 			eval := &structs.Evaluation{
 				ID:          structs.GenerateUUID(),
 				Priority:    job.Priority,
@@ -176,9 +175,12 @@ func (c *SimulatorCommand) Run(args []string) int {
 				JobID:       job.ID,
 			}
 
-			startTimestamp := int64(time.Now().UnixNano())
-
+			// Lock the mutex. This will make safe the Job scheduling operations.
+			// Without this, and while using the harness logic, a problem arises in
+			// which two Jobs are being concurrently allocated and they result in a
+			// situation of resource over-consumption for a given Node.
 			mutex.Lock()
+
 			// Process the evaluation, depending on the type of scheduler
 			// designated for the job. Modify this if a custom scheduler is
 			// to be used.
@@ -190,6 +192,8 @@ func (c *SimulatorCommand) Run(args []string) int {
 			case structs.JobTypeService:
 				noErr(h.Process(scheduler.NewServiceScheduler, eval))
 			}
+
+			// Unlock the mutex after scheduling is done for a particular Job.
 			mutex.Unlock()
 
 			// A snapshot of the simulator after each Job has been processed contains the
@@ -219,7 +223,6 @@ func (c *SimulatorCommand) Run(args []string) int {
 
 	// This is the metrics evaluation loop.
 	for iteration := 0; iteration < len(jobs); iteration++ {
-		// Receive the simulated cluster snapshot for metrics extraction.
 		snapshot := <-snapshotChan
 
 		jobEvaluationMetrics := getMetrics(snapshot)
@@ -227,16 +230,15 @@ func (c *SimulatorCommand) Run(args []string) int {
 		jobEvaluations = append(jobEvaluations, jobEvaluationMetrics)
 	}
 
-	// Since Jobs are processed in separate goroutines, we first insert each of them
+	// Structure to serialize the final simulator output.
 	simulatorOutput := &SimulatorOutput{
 		Nodes:          outputNodes,
 		JobEvaluations: jobEvaluations,
 	}
 
 	marshald, _ := json.MarshalIndent(simulatorOutput, "", "\t")
-	err := ioutil.WriteFile(outFilePath, marshald, 0755)
+	err := ioutil.WriteFile(outFile, marshald, 0755)
 	noErr(err)
-
 	return 0
 }
 
